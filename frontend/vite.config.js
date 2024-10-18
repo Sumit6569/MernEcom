aeedfa19a927c9008afb4a06af23dc123bf30f50
sumit kumar <infosumitkumar3322@gmail.com>
2024-10-18 07:17:34 +0530

    config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://localhost:4000",
+        target: "https://mernecom-pgkp.onrender.com",
         changeOrigin: true,
       },
     },
