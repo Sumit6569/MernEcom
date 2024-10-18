34b62b54fb9a398a9ae6b4c6172a6d3a51ff23b9
SUMIT KUMAR <149156187+Sumit6569@users.noreply.github.com>
2024-10-18 15:37:38 +0530

    Update userAction.jsx
    Change in userAction GIVE full url in login

--- frontend/src/actions/userAction.jsx
+++ frontend/src/actions/userAction.jsx
@@ -37,7 +37,7 @@ export const login = (email, password) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `/api/v1/login`,
+      `https://mernecom-pgkp.onrender.com/api/v1/login`,
       { email, password },
       config
     );
