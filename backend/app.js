c18e06f67b4c82215e1d967a9130da2d00b463a8
sumit kumar <infosumitkumar3322@gmail.com>
2024-10-20 18:01:06 +0530

    Changes in vs code

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,19 @@ const cors = require("cors");
 const errorMiddleware = require("./middleware/error");
 const path = require("path");
 
+const allowedOrigins = [
+  "https://mernecom-1-frontend.onrender.com",
+  "http://localhost:5173",
+];
 
-
-app.use(cors()); // Enable CORS for all routes
-
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // Enable cookies for cross-origin requests
+  })
+);
+// Enable CORS for all routes
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,11 +36,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
-
-
-
-
-
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
